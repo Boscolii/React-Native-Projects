f278cee93beb273239e01218bba3cc918c160c80
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 22:29:11 -0300

    Ajustando meu perfil

--- app-router/app/index.js
+++ app-router/app/index.js
@@ -4,16 +4,23 @@ export default function Home() {
   const router = useRouter();
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>🏠 Home</Text>
+      <Text style={styles.titulo}>Henrique Boscoli</Text>
       <TouchableOpacity style={styles.botao} onPress={() => router.push('/sobre')}>
-        <Text style={styles.botaoTexto}>Ir para Sobre</Text>
+        <Text style={styles.botaoTexto}>Ver meu perfil</Text>
       </TouchableOpacity>
+
+
     </View>
+
   );
+  
 }
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#f5f5f5' },
-  titulo:    { fontSize: 32, fontWeight: 'bold', marginBottom: 24 },
+  titulo:    { fontSize: 40, fontWeight: 'bold', marginBottom: 24 },
   botao:     { backgroundColor: '#E83D84', padding: 16, borderRadius: 12 },
   botaoTexto:{ color: '#fff', fontSize: 16, fontWeight: '600' },
+  fotoSimulada: {backgroundColor: '#E93333', padding: 50, borderRadius: 999},
+  inicialNome: {color: '#000', fontSize: 50},
+
 });
--- app-router/app/sobre.js
+++ app-router/app/sobre.js
@@ -5,7 +5,8 @@ export default function Sobre() {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>ℹ️ Sobre</Text>
-      <Text style={styles.descricao}>Esse app foi feito na FIAP! 🚀</Text>
+      <Text style={styles.descricao}>CC - 2026</Text>
+       <Text style={styles.descricao}>CC - 2026</Text>
       <TouchableOpacity onPress={() => router.back()}>
         <Text style={styles.voltar}>← Voltar</Text>
       </TouchableOpacity>
@@ -13,8 +14,8 @@ export default function Sobre() {
   );
 }
 const styles = StyleSheet.create({
-  container:  { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' },
+  container:  { flex: 1,  alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' },
   titulo:     { fontSize: 28, fontWeight: 'bold', marginBottom: 12 },
-  descricao:  { fontSize: 16, color: '#555', marginBottom: 24 },
+  descricao:  { fontSize: 20, color: '#555', marginBottom: 24 },
   voltar:     { fontSize: 16, color: '#E83D84', fontWeight: '600' },
 });
